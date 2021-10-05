404987a11aa8aa3ccea84993794c425624c14f75
trang17 <34981161+trang17@users.noreply.github.com>
2021-10-05 14:52:32 +0700

    1b

---
+++ ps1/ps1c.py
@@ -0,0 +1,18 @@
+current_saving = 0
+r = 0.04
+payment = 1000000 * 0.25
+semi_annualy_raise = 0.07
+Saving_time = 36
+
+#user input
+starting_salary = float(input("Enter starting salary "))
+
+# this ps use bisection search
+# https://youtu.be/BuwjGi8J5iA
+epsilon = 100
+low = 0.00
+high = 1.00
+salary_to_save = (high + low) / 2
+number_of_guesses = 0
+
+while 
